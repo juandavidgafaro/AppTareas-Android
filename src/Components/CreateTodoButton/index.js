f91620fb4aac57427f87f42eb1897197e61542b3
David Garcia <davidgarcia27737@gmail.com>
2023-08-20 18:07:08 -0500

    Se sube proyecto

---
+++ src/Components/CreateTodoButton/index.js
@@ -0,0 +1,16 @@
+import '../../Styles/CreateTodoButton.css';
+
+function CreateTodoButton({ setOpenModal }) {
+  return (
+    <button
+      className="CreateTodoButton"
+      onClick={
+        () => {
+          setOpenModal(state => !state);
+        }
+      }
+    >+</button>
+  );
+}
+
+export { CreateTodoButton };
---
+++ src/Components/TodoList/index.js
@@ -0,0 +1,12 @@
+import '../../Styles/TodoList.css'
+
+function TodoList({ children }) {
+  return (
+    <ul className="TodoList">
+      {children}
+    </ul>
+  );
+}
+
+export { TodoList };
+
